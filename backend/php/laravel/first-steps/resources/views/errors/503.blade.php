134ac2552fa419b629ce05f3cae1f2339b490581
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2017-09-25 20:44:36 -0500

    Laravel: Vistas de error por defecto

---
+++ backend/php/laravel/first-steps/resources/views/errors/503.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INSPINIA | 503 Error</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="gray-bg">
+    <div class="middle-box text-center animated fadeInDown">
+        <h1>503</h1>
+        <h3 class="font-bold">Internal Server Error</h3>
+    </div>
+    <!-- Mainly scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
