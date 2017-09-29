9a50e26187dbfe08233220ad6d70821d68700fcf
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2017-09-28 19:57:26 -0500

    Laravel: Manejo de rutas

--- backend/php/laravel/first-steps/routes/web.php
+++ backend/php/laravel/first-steps/routes/web.php
@@ -3,4 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController');
+
 Route::get('/about', 'AboutController');
+
+Route::match(['get', 'post'], '/multi', function () {
+    return 'Multiples Métodos';
+});
+
+Route::get('/echo/{word}', function ($word) {
+    return sprintf('Said %s', $word);
+});
+
+Route::get('/said/{word?}', function ($word = 'Jossie') {
+    return sprintf('Said %s', $word);
+});
+
+Route::get('user/{id}', function ($id) {
+    return $id;
+})->where('id', '[0-9]+');
+
+Route::prefix('admin')->group(function () {
+    Route::get('users', function () {
+        return '/admin/users';
+    });
+});
