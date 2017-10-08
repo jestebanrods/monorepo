cdaa838875acac5ccc9292dd9d0a5e30e8668e22
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2017-10-08 16:01:38 -0500

    Scripts: Automatización de registro de PPA

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sudo ansible-playbook -i hosts.cfg $1 
+sudo ansible-playbook -i hosts.cfg playbook.ppa.yml
+sudo ansible-playbook -i hosts.cfg playbook.languajes.yml
+sudo ansible-playbook -i hosts.cfg playbook.docker.yml
+sudo ansible-playbook -i hosts.cfg playbook.software.yml
