c35d5e2fe3566a7f85850e801e0a7a6a1f121649
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2018-01-20 13:03:40 -0500

    Node: Configuración de variables de entorno + Conexión a bbdd psql

--- backend/nodejs/websocket-chat/server.js
+++ backend/nodejs/websocket-chat/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 //Prototipo de mensaje a enviar
 let messages = [{
@@ -17,7 +17,9 @@ app.use(express.static(__dirname + '/public'));
 
 //Definición de la ruta hello
 app.get('/hello', (request, response) => {
-  response.status(200).send("Hello World!");
+  response.status(200).send(`
+    Hello World! the database name is ${process.env.DATABASE_NAME}
+  `);
 });
 
 //Abrir el servidor de websockets
@@ -34,3 +36,34 @@ io.on('connection', (socket) => {
     io.sockets.emit('messages', messages);
   });
 });
+
+//Conexión a bbdd postgresql
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+const client = new pg.Client(connectionString);
+
+/**
+const client = new pg.Client({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABSE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+})
+*/
+
+client.connect()
+
+client.query("CREATE TABLE IF NOT EXISTS personas(nombre varchar(64), apellido varchar(64), matriculado boolean)")
+  .catch(e => console.error(e.stack))
+
+client.query("INSERT INTO personas (nombre, apellido, matriculado) VALUES ($1, $2, $3)", ['Karen', 'Murillo', true])
+  .catch(e => console.error(e.stack))
+
+client.query("SELECT * FROM personas")
+  .then(res => {
+    res.rows.forEach((item) => {
+      console.log(item)
+    })
+  })
+  .catch(e => console.error(e.stack))
