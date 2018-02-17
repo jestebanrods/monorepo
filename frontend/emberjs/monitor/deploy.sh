60ab5857379a0f861507c30f2c158635181cced7
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2018-02-16 22:43:53 -0500

    Borrado de archivos y carpetas inncesarias

--- frontend/emberjs/monitor/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ember build --env production
-sudo rm -rf /var/www/html/
-sudo mkdir /var/www/html/
-sudo scp -r dist/* /var/www/html/
--- frontend/emberjs/started/app/routes/simcards/show.js
+++
@@ -1,7 +0,0 @@
-import Ember from "ember";
-
-export default Ember.Route.extend({
-  model(params){
-    return {show_id: params.show_id};
-  }
-});
