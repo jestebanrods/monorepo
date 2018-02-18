c492f0dd50ff3d3e45318061fce5fd280351c3b6
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2018-02-17 21:00:24 -0500

    Go: Server unificación de Dockerfiles

--- backend/go/server/compile.sh
+++ backend/go/server/compile.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-#Borrar todos los contenedores
 docker rm -f $(docker ps -a -q)
-
-#Construir la imagen del builder
-docker build -f Dockerfile.builder -t jestebanrods/go-builder .
-
-#Crear el contenedor que compilara
-docker run --name compilador jestebanrods/go-builder
-
-#Extraer el archivo compilado
-docker container cp compilador:/go/src/main ./bin/main
-
-#Crear la imagen del proyecto
-docker build -f Dockerfile -t jestebanrods/go-server .
-
-#Ejecutar la aplicación
+docker build -t jestebanrods/go-server .
 docker run -d -p 80:8000 --name app jestebanrods/go-server
