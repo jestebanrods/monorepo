4c13aef67fe795f99b0c522a9bd88411984eb8b0
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2018-04-29 23:10:50 -0500

    Vue: Ejecución de métodos y manejo de eventos

--- frontend/vue/primeros-pasos/main.js
+++ frontend/vue/primeros-pasos/main.js
@@ -25,6 +25,39 @@ const mv = new Vue({
     computed: {
         formTitle() {
             return this.formType ? 'A' : 'B'
+        },
+        existsErrorMsg() {
+            if (this.msg.name !== '') {
+                return this.msg.name
+            }
+            if (this.msg.phone !== '') {
+                return this.msg.phone
+            }
+            if (this.msg.email !== '') {
+                return this.msg.email
+            }
+            if (this.msg.comment !== '') {
+                return this.msg.comment
+            }
+            return ''
+        }
+    },
+    methods: {
+        validateData(evt) {
+            let input = evt.target,
+                expression
+
+            if (input.pattern) {
+                let regex = new RegExp(input.pattern)
+                expression = !regex.exec(input.value)
+            } else {
+                expression = !input.value
+            }
+
+            this.msg[input.name] = expression ? `Dato incorrecto, ${input.title}` : ``
+        },
+        sendForm(evt) {
+
         }
     }
 })
