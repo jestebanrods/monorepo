7f56d149e0c0ff8590ed9d349928c03f69a7cc42
Jossie Esteban Rodríguez Salazar <jossie.rodriguez@losmoviles.com.co>
2019-03-01 08:39:42 -0500

    Android Intro: Creación del proyecto para aprender android

---
+++ android/intro/app/src/main/java/com/jestebanrods/intro/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jestebanrods.intro.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jestebanrods.intro.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
