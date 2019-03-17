465713ebb65b234f6283edfe91ff5d3543dbb922
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2019-03-17 17:18:21 -0500

    Scripts: Correcciones en vars y script de driver wifi

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install build-essential git
 git clone https://github.com/lwfinger/rtlwifi_new/
+
 cd rtlwifi_new
+
 make
 sudo make install
 
 sudo modprobe -r rtl8723be
-sudo modprobe rtl8723be
+sudo modprobe -v rtl8723be ant_sel=2
 
-echo "options rtl8723be fwlps=0" | sudo tee /etc/modprobe.d/rtl8723be.conf
+echo "options rtl8723be ant_sel=2" | sudo tee /etc/modprobe.d/50-rtl8723be.conf
+
