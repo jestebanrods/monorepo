f6d5c15be1161ba4d30490434b24db7680903a7b
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2019-03-24 23:23:57 -0500

    Depurando archivos + Eliminación de archivos innecesarios

--- backend/nginx/code.Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM php:7-fpm-alpine
-COPY src /src
-EXPOSE 9000
--- backend/nodejs/core-modules/modules.js
+++
@@ -1,6 +0,0 @@
-let name = 'Jossie Esteban',
-    email = 'jers.28@hotmail.com',
-    _phone = 3211231233
-
-module.exports.name = name
-module.exports.email = email
