c158c0013bea78e396fc7552c4f5d0a02997b245
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2019-03-24 23:23:57 -0500

    Depurando archivos + Eliminación de archivos innecesarios

--- backend/nginx/server.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:1.12-alpine
-COPY src /src
-COPY default.conf /etc/nginx/conf.d
-EXPOSE 80
--- backend/nodejs/core-modules/process.js
+++
@@ -1,9 +0,0 @@
-// El módulo process son las características del proceso de ejecución del script
-console.log(`Id Proceso: ${process.pid}`)
-console.log(`Tiempo Ejecución: ${process.uptime()}`)
-console.log(`Path Actual: ${process.cwd()}`)
-console.log(`Argumentos: ${process.argv}`)
-
-// Por defecto los argumentos pasados al archivos (argv) son: 
-// [0] ruta del ejecutable de node
-// [1] ruta del script a ejecutar
