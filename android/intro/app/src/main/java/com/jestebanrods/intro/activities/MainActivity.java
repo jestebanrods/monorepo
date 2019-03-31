bb9ab9f3da618c5cd5f4600da763acefa84694bd
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2019-03-31 17:51:33 -0500

    Android Intro: Interacción layout-java

--- android/intro/app/src/main/java/com/jestebanrods/intro/activities/MainActivity.java
+++ android/intro/app/src/main/java/com/jestebanrods/intro/activities/MainActivity.java
@@ -2,14 +2,40 @@ package com.jestebanrods.intro.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.jestebanrods.intro.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView txtPrincipal;
+    private Button btnOk;
+
+    private Boolean flagText = false;
+    private String oldText = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Visualización de la interfaz gráfica
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Mapeo de los componente visuales
+        txtPrincipal = findViewById(R.id.txt_principal);
+        btnOk = findViewById(R.id.btn_ok);
+
+        oldText = txtPrincipal.getText().toString();
+
+        // Evento click del boton
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flagText = !flagText;
+                txtPrincipal.setText(flagText ? "Condiciones Aceptadas" : oldText);
+            }
+        });
     }
+
 }
