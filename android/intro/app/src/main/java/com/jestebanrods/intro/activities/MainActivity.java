6e58faab43560f5c2f02fffffa4800b4ff94ad8b
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2019-03-31 17:04:58 -0500

    Android Intro: Mejoras menores

--- android/intro/app/src/main/java/com/jestebanrods/intro/activities/MainActivity.java
+++ android/intro/app/src/main/java/com/jestebanrods/intro/activities/MainActivity.java
@@ -1,7 +1,7 @@
 package com.jestebanrods.intro.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.jestebanrods.intro.R;
 
