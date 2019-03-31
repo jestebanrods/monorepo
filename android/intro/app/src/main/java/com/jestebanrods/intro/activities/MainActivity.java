f97aae64d5570f2f7a80a99afac503ea2fe87166
Jossie Esteban Rodríguez Salazar <jestebanrods@gmail.com>
2019-03-31 18:02:35 -0500

    Android Intro: Manejo de eventos de botón + Toast

--- android/intro/app/src/main/java/com/jestebanrods/intro/activities/MainActivity.java
+++ android/intro/app/src/main/java/com/jestebanrods/intro/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jestebanrods.intro.R;
 
@@ -28,12 +29,21 @@ public class MainActivity extends AppCompatActivity {
 
         oldText = txtPrincipal.getText().toString();
 
-        // Evento click del boton
+        // Evento click del botón
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 flagText = !flagText;
                 txtPrincipal.setText(flagText ? "Condiciones Aceptadas" : oldText);
+            }
+        });
+
+        // Evento presionado largo tiempo del botón
+        btnOk.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Presionando", Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
     }
